a30cb5312b669a46830a675f26b90b19ca49e0c7
lacha.arg@gmail.com <lacha.arg@gmail.com>
2022-07-27 22:13:06 -0300

    Create a multi-color theme for the calcultator and added event listeners to the toggle to switch themes

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 "use strict";
+const toggler = document.querySelector(".header__themes--toggle");
+const themeOne = document.querySelector(".one");
+const themeTwo = document.querySelector(".two");
+const themeThree = document.querySelector(".three");
+let colorTheme;
+
 function sum(n1, n2) {
   return n1 + n2;
 }
@@ -26,3 +32,35 @@ function operate(operator, n1, n2) {
     return divide(n1, n2);
   }
 }
+
+const changeColorTheme = function () {
+  if (colorTheme === 1) {
+    toggler.classList.remove("theme-two");
+    toggler.classList.remove("theme-three");
+    document.body.classList.remove("theme-two");
+    document.body.classList.remove("theme-three");
+  } else if (colorTheme === 2) {
+    toggler.classList.remove("theme-three");
+    document.body.classList.remove("theme-three");
+    toggler.classList.add("theme-two");
+    document.body.classList.add("theme-two");
+  } else if (colorTheme === 3) {
+    toggler.classList.add("theme-three");
+    document.body.classList.add("theme-three");
+  }
+};
+
+themeOne.addEventListener("click", () => {
+  colorTheme = 1;
+  changeColorTheme();
+});
+
+themeTwo.addEventListener("click", () => {
+  colorTheme = 2;
+  changeColorTheme();
+});
+
+themeThree.addEventListener("click", () => {
+  colorTheme = 3;
+  changeColorTheme();
+});
