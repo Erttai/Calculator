9dcf53e261305ae84dbbc7df284db54288299f5b
lacha.arg@gmail.com <lacha.arg@gmail.com>
2022-07-28 23:57:06 -0300

    Made some functions to store the number and print it in the calc display

--- script.js
+++ script.js
@@ -3,8 +3,48 @@ const toggler = document.querySelector(".header__themes--toggle");
 const themeOne = document.querySelector(".one");
 const themeTwo = document.querySelector(".two");
 const themeThree = document.querySelector(".three");
+const numberBtn = document.querySelectorAll(".number");
+const displayScreen = document.querySelector(".calc-screen");
+const operatorBtn = document.querySelectorAll(".operator");
 let colorTheme;
 
+//
+//Color Themes
+const changeColorTheme = function () {
+  if (colorTheme === 1) {
+    toggler.classList.remove("theme-two");
+    toggler.classList.remove("theme-three");
+    document.body.classList.remove("theme-two");
+    document.body.classList.remove("theme-three");
+  } else if (colorTheme === 2) {
+    toggler.classList.remove("theme-three");
+    document.body.classList.remove("theme-three");
+    toggler.classList.add("theme-two");
+    document.body.classList.add("theme-two");
+  } else if (colorTheme === 3) {
+    toggler.classList.add("theme-three");
+    document.body.classList.add("theme-three");
+  }
+};
+
+//Eventlisteners to change calc's color theme
+themeOne.addEventListener("click", () => {
+  colorTheme = 1;
+  changeColorTheme();
+});
+
+themeTwo.addEventListener("click", () => {
+  colorTheme = 2;
+  changeColorTheme();
+});
+
+themeThree.addEventListener("click", () => {
+  colorTheme = 3;
+  changeColorTheme();
+});
+
+//
+//Math functions
 function sum(n1, n2) {
   return n1 + n2;
 }
@@ -23,44 +63,55 @@ function divide(n1, n2) {
 
 function operate(operator, n1, n2) {
   if (operator === "+") {
-    return sum(n1, n2);
+    const total = sum(n1, n2);
+    displayScreen.textContent = `${convertStr(total)}`;
   } else if (operator === "-") {
-    return subtract(n1, n2);
-  } else if (operator === "*") {
-    return multiply(n1, n2);
+    const total = subtract(n1, n2);
+    displayScreen.textContent = `${convertStr(total)}`;
+  } else if (operator === "x") {
+    const total = multiply(n1, n2);
+    displayScreen.textContent = `${convertStr(total)}`;
   } else if (operator === "/") {
-    return divide(n1, n2);
+    const total = divide(n1, n2);
+    displayScreen.textContent = `${convertStr(total)}`;
   }
 }
 
-const changeColorTheme = function () {
-  if (colorTheme === 1) {
-    toggler.classList.remove("theme-two");
-    toggler.classList.remove("theme-three");
-    document.body.classList.remove("theme-two");
-    document.body.classList.remove("theme-three");
-  } else if (colorTheme === 2) {
-    toggler.classList.remove("theme-three");
-    document.body.classList.remove("theme-three");
-    toggler.classList.add("theme-two");
-    document.body.classList.add("theme-two");
-  } else if (colorTheme === 3) {
-    toggler.classList.add("theme-three");
-    document.body.classList.add("theme-three");
-  }
+function convertStr(n) {
+  return n.toString().replace(".", ",");
+}
+
+const parsedNumber = function (array) {
+  return parseFloat(array.join(""), 10);
 };
 
-themeOne.addEventListener("click", () => {
-  colorTheme = 1;
-  changeColorTheme();
-});
+//
+//Printing number in calc's display
 
-themeTwo.addEventListener("click", () => {
-  colorTheme = 2;
-  changeColorTheme();
-});
+let arr = [];
+const storedNumbers = [];
+let mathOperation = "";
 
-themeThree.addEventListener("click", () => {
-  colorTheme = 3;
-  changeColorTheme();
-});
+numberBtn.forEach(btn =>
+  btn.addEventListener("click", e => {
+    let int = e.target.textContent;
+    arr.push(int);
+    int = parsedNumber(arr);
+    displayScreen.textContent = `${convertStr(int)}`;
+  })
+);
+
+operatorBtn.forEach(btn =>
+  btn.addEventListener("click", e => {
+    storedNumbers.push(parsedNumber(arr));
+
+    arr = [];
+
+    const str = e.target.textContent;
+    mathOperation = str;
+
+    if (storedNumbers.length === 2) {
+      operate(mathOperation, ...storedNumbers);
+    }
+  })
+);
